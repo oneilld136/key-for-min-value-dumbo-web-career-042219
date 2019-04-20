2ae3d8be4492649dbff2c8bcb468deb17d131608
oneilld136 <oneilld136@gmail.com>
2019-04-20 21:12:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+hash=0
+  value = {
+    :blake =>500,
+    :ashley=>2,
+    :adam=>1
+  }
+  name_hash.each do |name, hash|
+    if hash<2
+      puts name_hash
+end
 end
