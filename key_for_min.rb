bcf805df5837ce4378f54f0b6f45e5a4ed813725
oneilld136 <oneilld136@gmail.com>
2019-04-20 21:16:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,13 +9,13 @@ name_hash = {
   }
 
   def key_for_min_value(name_hash)
-    lowest_key = nil 
-    lowest_value = nil 
+    lowest_key = nil
+    lowest_value = nil
     name_hash.each do |k, v|
       if lowest_value == nil || v < lowest_value
-        lowest_value = v 
-        lowest_key = k 
-      end 
-    end 
+        lowest_value = v
+        lowest_key = k
+      end
+    end
     lowest_key
   end 
